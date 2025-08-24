71c2436a4b8cec32a2bf5ae7ccbafa2431792f82
Andrew Walker <andrewwalker1239@gmail.com>
2025-08-23 21:38:57 -0400

    Chapter 3 Finished

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 50
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+          let alert = UIAlertController(
+            title: "Hello, World",
+            message: message,    // changed
+            preferredStyle: .alert)
+          let action = UIAlertAction(
+            title: "OK",          // changed
+            style: .default,
+            handler: nil)
+          alert.addAction(action)
+          present(alert, animated: true, completion: nil)
+    }
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
 
